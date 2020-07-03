60983ab3df43ad5b7628722d010991dd40248fb3
tejaswinim <tejaswinir0@gmail.com>
2020-07-03 12:55:11 +0530

    Recipes app

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package: recipes_app/ui/screens/login.dart';
+
+class RecipesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Recipes',
+        initialRoute: '/login',
+        routes: {
+
+
+          '/': (context) => LoginScreen(),
+          '/login': (context) => LoginScreen(),
+        }
+    );
+
+
+  }
+}
---
+++ lib/ui/screens/login.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extend StatelessWidget{}
